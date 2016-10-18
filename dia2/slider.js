48ac65c05b5d73ffc0762763be979e72f0459f50
wasabiBD <cbarrosd@gmail.com>
2016-10-18 17:03:43 +0100

    Update slider.js

--- dia2/slider.js
+++ dia2/slider.js
@@ -1,4 +1,12 @@
-/* Activación del slider */
+/* Activación del slider */ /*
 $(document).ready(function() {
   $('.slider').slick();
+}); */
+
+/* Activación del slider CON AUTOPLAY */
+$(document).ready(function() {
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
 });
