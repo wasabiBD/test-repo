288ad9906aa18efac481c8e18ad807fd1fe63e8b
wasabiBD <cbarrosd@gmail.com>
2016-10-19 19:17:05 +0100

    Update index.js

--- dia3/editor/index.js
+++ dia3/editor/index.js
@@ -1,6 +1,8 @@
+$(document).ready(function() {
 $('.editor').trumbowyg({
     svgPath: 'icons.svg', // path of icons.svg
     // Habitualmente, se indica la ruta del SVG en este parámetro, pero
     // en codepen, simplemente pegaremos el contenido del SVG en la parte
     // del código HTML
+});
 });
