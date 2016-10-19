40d42ef05faa3e2bbbb3ae4ffb38515fbb82125e
wasabiBD <cbarrosd@gmail.com>
2016-10-19 18:59:42 +0100

    Update index.js

--- dia3/editor/index.js
+++ dia3/editor/index.js
@@ -1,5 +1,5 @@
 $('.editor').trumbowyg({
-    // svgPath: '/icons/icons.svg', // path of icons.svg
+    svgPath: 'icons.svg', // path of icons.svg
     // Habitualmente, se indica la ruta del SVG en este parámetro, pero
     // en codepen, simplemente pegaremos el contenido del SVG en la parte
     // del código HTML
