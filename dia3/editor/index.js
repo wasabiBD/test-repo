f77f523bb528d1b496153a2049199ed5e190dee3
wasabiBD <cbarrosd@gmail.com>
2016-10-19 18:55:50 +0100

    Create index.js

---
+++ dia3/editor/index.js
@@ -0,0 +1,6 @@
+$('.editor').trumbowyg({
+    // svgPath: '/icons/icons.svg', // path of icons.svg
+    // Habitualmente, se indica la ruta del SVG en este parámetro, pero
+    // en codepen, simplemente pegaremos el contenido del SVG en la parte
+    // del código HTML
+});
